81ce052f90b72a007858b879f55c0aa525fe4acd
chuhuy <45164683+chuhuy@users.noreply.github.com>
2020-12-09 11:06:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
-const express = require('express');
-const cors = require('cors');
-const hpp = require('hpp');
-const dotenv = require('dotenv').config();
-const bodyParser = require('body-parser');
-
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
+const app = require('express')();
+const server = require('http').createServer(app);
+const options = { /* ... */ };
+const io = require('socket.io')(server, options);
 const port = process.env.PORT || 5000;
+
+io.on('connection', socket => {
+    console.log(socket.id)
+    socket.on("private message", (anotherSocketId, msg) => {
+        socket.to(anotherSocketId).emit("private message", socket.id, msg);
+      });
+});
 
 app.get('/', (req, res) => {
     res.send("Music Life Comment Server");
-});
-
-io.on('connection', (socket) => {
-    console.log('A user connected');
-    socket.on('1', (msg) => {
-        console.log('message: ' + msg);
-    });
-    socket.emit("broadcast", "hello friends!");
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
 })
 
-http.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
+
+// const express = require('express');
+// const cors = require('cors');
+// const hpp = require('hpp');
+// const dotenv = require('dotenv').config();
+// const bodyParser = require('body-parser');
+
+// const app = express();
+// const http = require('http').createServer(app);
+// const io = require('socket.io')(http);
+
+// const port = process.env.PORT || 5000;
+
+// app.get('/', (req, res) => {
+//     res.send("Music Life Comment Server");
+// });
+
+// io.on('connection', (socket) => {
+//     console.log('A user connected');
+//     socket.on('1', (msg) => {
+//         console.log('message: ' + msg);
+//     });
+//     socket.emit("broadcast", "hello friends!");
+//     socket.on('disconnect', () => {
+//         console.log('user disconnected');
+//     });
+// })
+
+// http.listen(port, () => {
+//     console.log(`Server is listening on port ${port}`);
+// });
+
